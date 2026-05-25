cc4701b19e13069c2ab0af814cde14af46b417ce
Willian490 <williandeoliveiramarquesolivei@gmail.com>
2026-05-25 16:26:35 -0300

    Criação da view dedicada a listagem das Publicações de Denúncias

---
+++ App/View/dashboard/publicacao_encontrado_listar.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+
+<div class="container-fluid">
+    <div class="d-flex justify-content-between align-items-center mb-4">
+        <h1 class="h3 mb-0">Listar Publicação de Animais Encontrados</h1>
+        <a href="/dashboard/publicacao_encontrado/cadastro" class="btn btn-primary">
+            <i class="fas fa-plus"></i> Nova Publicação de Animal Encontrado
+        </a>
+    </div>
+
+    <div class="card shadow">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="tabela-publicacao_encontrado" class="table table-striped table-hover" width="100%">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>Fk animal id</th>
+                            <th>Fk login id</th>
+                            <th>Data Encontro</th>
+                            <th>Condição Fisica</th>
+                            <th>Ações Realizadas</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+                        
+                    </tbody>
+                </table>                    
+            </div>
+        </div>
+    <div>
+<div>
