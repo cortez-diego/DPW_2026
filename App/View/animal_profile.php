6b6c4d26456634642ea5be9543c64f30c094738f
tchrrmartins <prismavoltant@gmail.com>
2026-05-25 22:43:41 -0300

    Perfil Animal

---
+++ App/View/animal_profile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * AmigoPet - Dashboard Principal
+ * Localização: ~/App/View/dashboard.php
+ * Este arquivo serve como o "Layout" ou "Template" principal.
+ */
+
+// Caminhos corrigidos para a pasta de includes
+include 'includes/dashboard/header.php';
+include 'includes/dashboard/menu.php';
+include 'includes/dashboard/navbar.php';
+
+include 'animal_profile_content.php';
+
+include 'includes/dashboard/footer.php';
+?>
