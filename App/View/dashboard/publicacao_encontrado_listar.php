a8e07c25aefea2f8af5fbfbf9d85249573cc1f9d
WillianOliveira <williandeoliveiramarquesolivei@gmail.com>
2026-06-05 14:54:15 -0300

    Alteração de redirecionamento na rota

--- App/View/dashboard/publicacao_encontrado_listar.php
+++ App/View/dashboard/publicacao_encontrado_listar.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
     <div class="d-flex justify-content-between align-items-center mb-4">
         <h1 class="h3 mb-0">Listar Publicação de Animais Encontrados</h1>
-        <a href="/dashboard/publicacao_encontrado/cadastro" class="btn btn-primary">
+        <a href="/dashboard/publicacao/cadastro" class="btn btn-primary">
             <i class="fas fa-plus"></i> Nova Publicação de Animal Encontrado
         </a>
     </div>
