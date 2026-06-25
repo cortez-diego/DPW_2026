14ec0a94b602938832cbdb984bbdc153d826178b
lbonatoc <luccabonatoc@gmail.com>
2026-06-25 11:20:55 -0300

    Correção do metodo cadastrar de AdotanteController.

--- App/Controller/AdotanteController.php
+++ App/Controller/AdotanteController.php
@@ -129,6 +129,9 @@ class AdotanteController extends Action
         } catch(CpfJaCadastradoException $ex) {
             header('Location: /dashboard/adotante/cadastro?erro=cpf-cadastrado');
             die();
+        }catch(\PDOException $ex) {
+            header('Location: /dashboard/adotante/cadastro?erro=cadastro');
+            die();
         } catch(\Throwable $ex) {
             header('Location: /dashboard/adotante/cadastro?erro=inesperado');
             die();
